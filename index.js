466c25919d8de88b40421089821dbdeb672ebe48
Michael <michael.d.lange88@gmail.com>
2022-10-22 21:04:38 -0700

    commit tree 9cf8a4898c3135874918f2d5d5b5841aaab409f4

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Add your code here
+// submitData function, takes two strings as arguments, one representing a user's name, and the other an email.
+// write so the fetch() is returned inside submitData
+
+function submitData(user, userEmail){
+    const userData = {
+        name: user,
+        email: userEmail
+    }
+    const configInfo = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userData),
+    };
+    return fetch("http://localhost:3000/users", configInfo)
+    .then(res => res.json())
+    .then(function updateDom(object){
+        let body = document.querySelector('body')
+        body.innerHTML = `
+            <div class="user_data">
+                <h1>${user}</h1>
+                <h2>${userEmail}</h2>
+                <h3>${object.id}</h3>
+            </div>`
+    })
+    .catch(function (error){
+        alert("You don did it now!")
+        let badbody = document.querySelector('body')
+        badbody.innerHTML = error.message
+})
+}
